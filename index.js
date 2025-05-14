936998cc346cdde12d2b0300afdfcbfcbb04b726
Gian <gian.2000@hotmail.com>
2025-05-14 03:38:14 -0300

    update postman files

--- index.js
+++ index.js
@@ -29,12 +29,14 @@ app.get('/', (req, res) => {
 
 const DB_USER = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
+const MongoURL = process.env.MONGO_URL
 
 //entregar uma porta
 mongoose
     .connect(
         //Conectar usando um arquivo .env
-        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@api-parte1.pqv2ajh.mongodb.net/?retryWrites=true&w=majority&appName=api-parte1`
+        //`mongodb+srv://${DB_USER}:${DB_PASSWORD}@api-parte1.pqv2ajh.mongodb.net/?retryWrites=true&w=majority&appName=api-parte1`
+        MongoURL
     )
     .then(() =>{
         console.log("Conectamos ao MongoDB!")
